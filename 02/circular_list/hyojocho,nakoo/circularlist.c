80f91e830a7e12d5d187a8f4c6e433e3a35f0703
Namkyung Koo <nakoo@c10r6s14.42seoul.kr>
2022-12-11 18:23:16 +0900

    Debug removeElement function

--- 02/circular_list/hyojocho,nakoo/circularlist.c
+++ 02/circular_list/hyojocho,nakoo/circularlist.c
@@ -1,129 +1,153 @@
-//
-// Created by sanguk on 04/08/2017.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hyo_koo_circularlist.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nakoo <nakoo@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/10 19:23:54 by hyojocho          #+#    #+#             */
+/*   Updated: 2022/12/11 18:18:47 by nakoo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "circularlist.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
+List	*createList(void)
+{
+	List	*pList;
 
-List *createList() {
-    List *pList = NULL;
-    pList = (List *) malloc(sizeof(List));
-    if (pList == NULL) return NULL;
-    memset(pList, 0, sizeof(List));
-    pList->currentElementCount = 0;
-    pList->pLink = NULL;
-    return pList;
+	pList = (List *)malloc(sizeof(List));
+	if (!pList)
+		return (NULL);
+	memset(pList, 0, sizeof(List));
+	return (pList);
 }
 
-void deleteList(List *pList) {
-    if (pList == NULL) return;
-    clearList(pList);
-    free(pList);
+void	deleteList(List *pList)
+{
+	free(pList);
 }
 
-int addElement(List *pList, int position, ListNode node) {
-    if (pList == NULL) return FALSE;
-    if (position < 0 || position > getListLength(pList)) return FALSE;
-    ListNode *pNewNode = NULL, *pPreNode = NULL, *pLastNode;
-    pNewNode = (ListNode *) malloc(sizeof(ListNode));
-    if (pNewNode == NULL) return FALSE;
+int	addElement(List *pList, int position, ListNode node)
+{
+	ListNode	*new_node;
+	ListNode	*tmp;
 
-    *pNewNode = node;
-    pNewNode->pLink = NULL;
-
-    if (position == 0) {
-        if (getListLength(pList) == 0) {
-            pList->pLink = pNewNode;
-            pNewNode->pLink = pNewNode;
-        } else {
-            pLastNode = pList->pLink;
-            while (pLastNode->pLink != pList->pLink) {
-                pLastNode = pLastNode->pLink;
-            }
-            pList->pLink = pNewNode;
-            pNewNode->pLink = pLastNode->pLink;
-            pLastNode->pLink = pNewNode;
-        }
-    } else {
-        pPreNode = pList->pLink;
-        for (int i = 0; i < position - 1; i++) {
-            pPreNode = pPreNode->pLink;
-        }
-        pNewNode->pLink = pPreNode->pLink;
-        pPreNode->pLink = pNewNode;
-    }
-    pList->currentElementCount++;
-
-    return TRUE;
+	if (position < 0 || position > pList->currentElementCount)
+		return (FALSE);
+	new_node = (ListNode *)malloc(sizeof(ListNode));
+	if (!new_node)
+		return (FALSE);
+	new_node->data = node.data;
+	tmp = pList->pLink;
+	if (position == 0)
+	{
+		if (pList->currentElementCount == 0)
+		{
+			pList->pLink = new_node;
+			new_node->pLink = new_node;
+		}
+		else
+		{
+			while (tmp->pLink != pList->pLink)
+				tmp = tmp->pLink;
+			pList->pLink = new_node;
+			new_node->pLink = tmp->pLink;
+			tmp->pLink = new_node;
+		}
+	}
+	else
+	{
+		while (position - 1 > 0)
+		{
+			tmp = tmp->pLink;
+			position--;
+		}
+		new_node->pLink = tmp->pLink;
+		tmp->pLink = new_node;
+	}
+	pList->currentElementCount++;
+	return (TRUE);
 }
 
-int removeElement(List *pList, int position) {
-    if (pList == NULL) return FALSE;
-    if (position < 0 || position > getListLength(pList)) return FALSE;
-    ListNode *pDelNode = NULL;
-    if (position == 0) {
-        pDelNode = pList->pLink;
-        if (getListLength(pList) == 1) {
-            free(pDelNode);
-            pList->pLink = NULL;
-        } else {
-            ListNode *pLast = pList->pLink;
-            while (pLast->pLink != pList->pLink) {
-                pLast = pLast->pLink;
-            }
-            pLast->pLink = pDelNode->pLink;
-            pList->pLink = pDelNode->pLink;
-            free(pDelNode);
-        }
-    } else {
-        ListNode *pPreNode = pList->pLink;
-        for (int i = 0; i < position - 1; i++) {
-            pPreNode = pPreNode->pLink;
-        }
-        pDelNode = pPreNode->pLink;
-        pPreNode->pLink = pDelNode->pLink;
-        free(pDelNode);
-    }
-    pList->currentElementCount--;
+void	displayList(List *pList)
+{
+	ListNode	*tmp;
+	int			cnt;
 
-    return TRUE;
+	tmp = pList->pLink;
+	cnt = pList->currentElementCount;
+	printf("displayList\n");
+	while (cnt > 0)
+	{
+		printf("%d ", tmp->data);
+		tmp = tmp->pLink;
+		cnt--;
+	}
+	printf("\n");
 }
 
-ListNode *getElement(List *pList, int position) {
-    ListNode *pNode;
-    if (pList == NULL) return NULL;
-    if (position < 0 || position > getListLength(pList)) return NULL;
-    pNode = pList->pLink;
-    for (int i = 0; i < position; i++) {
-        pNode = pNode->pLink;
-    }
-    return pNode;
+int	removeElement(List *pList, int position)
+{
+	ListNode	*tmp;
+	ListNode	*removeNode;
+
+	if (position < 0 || position > pList->currentElementCount)
+		return (FALSE);
+	tmp = pList->pLink;
+	if (position == 0)
+	{
+		if (pList->currentElementCount == 1)
+		{
+			free(pList->pLink);
+			pList->pLink = NULL;
+		}
+		else
+		{
+			while (tmp->pLink != pList->pLink)
+				tmp = tmp->pLink;
+			tmp->pLink = pList->pLink->pLink;
+			free(pList->pLink);
+			pList->pLink = tmp->pLink;
+		}
+	}
+	else
+	{
+		while (position - 1 > 0)
+		{
+			tmp = tmp->pLink;
+			position--;
+		}
+		removeNode = tmp->pLink;
+		tmp->pLink = removeNode->pLink;
+		free(removeNode);
+	}
+	pList->currentElementCount--;
+	return (TRUE);
 }
 
-int getListLength(List *pList) {
-    if (pList == NULL) return 0;
-    return pList->currentElementCount;
+ListNode	*getElement(List *pList, int position)
+{
+	ListNode	*tmp;
+
+	tmp = pList->pLink;
+	while (position > 0)
+	{
+		tmp = tmp->pLink;
+		position--;
+	}
+	return (tmp);
 }
 
-void displayList(List *pList) {
-    printf("displayList\n");
-    ListNode *pNode;
-    for (int i = 0; i < getListLength(pList); i++) {
-        pNode = getElement(pList, i);
-        if (pNode != NULL) {
-            printf("%d ", pNode->data);
-        }
-    }
-    printf(", first node data : %d ", pNode->pLink->data);
-    printf("\n");
+int	getListLength(List *pList)
+{
+	return (pList->currentElementCount);
 }
 
-void clearList(List *pList) {
-    if (pList == NULL) return;
-    while (getListLength(pList) != 0) {
-        removeElement(pList, 0);
-    }
+void	clearList(List *pList)
+{
+	while (pList->currentElementCount > 0)
+	{
+		removeElement(pList, 0);
+	}
 }
