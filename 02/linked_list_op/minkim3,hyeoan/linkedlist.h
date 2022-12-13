2a63d9513a3325a7cddcb84a2045e19f435942c0
Hyeon Jun An <hyeoan@c4r7s4.42seoul.kr>
2022-12-13 17:54:20 +0900

    feat(linkedlist) : linkedlist

---
+++ 02/linked_list_op/minkim3,hyeoan/linkedlist.h
@@ -0,0 +1,33 @@
+#ifndef LINKED_LIST_OP_LINKEDLIST_H
+#define LINKED_LIST_OP_LINKEDLIST_H
+
+#define TRUE        1
+#define FALSE        0
+
+typedef struct ListNodeType {
+    int data;
+    struct ListNodeType *pLink;
+} ListNode;
+
+typedef struct ListType {
+    int currentElementCount;
+    ListNode headerNode;
+} List;
+
+List *createList();
+
+int addElement(List *pList, int position, ListNode element);
+
+int removeElement(List *pList, int position);
+
+ListNode *getElement(List *pList, int position);
+
+void clearList(List *pList);
+
+int getListLength(List *pList);
+
+void deleteList(List *pList);
+
+void displayList(List *pList);
+
+#endif //LINKED_LIST_OP_LINKEDLIST_H
