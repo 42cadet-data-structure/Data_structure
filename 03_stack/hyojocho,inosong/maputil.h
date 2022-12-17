26a0fa210625387f145a45444b44851d969f92d4
inohsong <inossong@naver.com>
2022-12-17 19:47:39 +0900

    ino

---
+++ 03_stack/hyojocho,inosong/maputil.h
@@ -0,0 +1,24 @@
+//
+// Created by sanguk on 08/08/2017.
+//
+
+#ifndef STACK_MAP_MAPUTIL_H
+#define STACK_MAP_MAPUTIL_H
+#define WIDTH 8
+#define HEIGHT 8
+#include "mapdef.h"
+#include "mapstack.h"
+
+void findPath(int mazeArray[HEIGHT][WIDTH],
+              MapPosition startPos,
+              MapPosition endPos,
+              Stack *pStack);
+
+// int pushMapPosition(Stack *pStack, MapPosition data);
+
+// void showPath(Stack *pStack, int mazeArray[HEIGHT][WIDTH]);
+
+void printMaze(int mazeArray[HEIGHT][WIDTH]);
+
+
+#endif //STACK_MAP_MAPUTIL_H
