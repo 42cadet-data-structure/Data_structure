7a492474c9f0225b4c6580e3fb12a6df23d47ba9
democracyKim <peppertones0123@gmail.com>
2023-01-20 12:23:26 +0900

    quick

--- 07_sort/inosong,minkim3/quick.c
+++ 07_sort/inosong,minkim3/quick.c
@@ -13,9 +13,35 @@ void swap(int *a, int *b){
     *a = temp;
 }
 
-int partitionQuickSort(int *values, int start, int end)
+int	return_key(int values[], int start, int end)
+{
+	int	pivot = start;
+	while (start < end )
+	{
+		//while (start < end && values[end] > values[pivot])
+		while (start < end && values[end] > values[pivot])
+			end--;
+		while (start < end && values[start] <= values[pivot])
+			start++;
+		swap(&values[start], &values[end]);
+		printArray(values);
+	}
+	swap(&values[pivot], &values[end]);
+
+	return (start);
+}
 
 void quickSort(int values[], int start, int end)
+{
+	int key;
+	if (start >= end)
+		return ;
+	key = return_key(values, start, end);
+	printArray(values);
+	quickSort(values, start, key - 1);
+	quickSort(values, key + 1, end);
+	
+}
 
 int main() {
     int values[] = {80, 50, 70, 10, 60, 20, 40, 30};
