7389f38d75e7b3e954c87a6745ab0a405ece9c73
jiwon0829 <hanjiwon8295@gmail.com>
2023-01-26 21:21:36 +0900

    init insertion_sort

--- 07_sort/insertion-sort/jiwonhan,hyeoan/insertion_sort.c
+++ 07_sort/insertion-sort/jiwonhan,hyeoan/insertion_sort.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void print_array(int *array, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", array[i]);
+    printf("\n");
+}
+
+void    insertion_sort(int *array, int n)
+{
+
+    print_array(array, n);
+}
 
 int main()
 {
