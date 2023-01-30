1b6aefc13dfd739a32368afedb79e72b1d6797b5
euiclee <lechan1317@gmail.com>
2023-01-30 13:03:47 +0900

    se search finish

--- 08_hash/seq-search/euiclee,hyeoan/seqsearch.h
+++ 08_hash/seq-search/euiclee,hyeoan/seqsearch.h
@@ -1,7 +1,3 @@
-//
-// Created by sanguk on 14/08/2017.
-//
-
 #ifndef SEQ_SEARCH_SEQSEARCH_H
 #define SEQ_SEARCH_SEQSEARCH_H
 
@@ -9,11 +5,11 @@
 #define FALSE 0
 #define FAIL -1
 
-int sequentialSearchNotSorted(int value[], int size, int key);
+int sequentialSearchNotSorted(int *value, int size, int key);
 
-int sequentialSearchAsendingSorted(int value[], int size, int key);
+int sequentialSearchAsendingSorted(int *value, int size, int key);
 
-void showArray(int value[], int size);
+void showArray(int *value, int size);
 
 
 #endif //SEQ_SEARCH_SEQSEARCH_H
