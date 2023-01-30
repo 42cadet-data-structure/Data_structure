f5318efba9858c58e1f21f47c43a84a024b55244
jiwon0829 <hanjiwon8295@gmail.com>
2023-01-30 17:13:36 +0900

    seq-search

--- 08_hash/seq-search/hyojocho,jiwonhan/seqsearch.c
+++ 08_hash/seq-search/hyojocho,jiwonhan/seqsearch.c
@@ -2,3 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include "seqsearch.h"
+
+int sequentialSearchNotSorted(int *value, int size, int key)
+{
+	for (int i=0;i<size;i++)
+		if (value[i] == key)
+			return (i);
+	return (FAIL);
+}
+
+int sequentialSearchAsendingSorted(int *value, int size, int key)
+{
+	for (int i=0;i<size && value[i] <= key;i++)
+		if (value[i] == key)
+			return (i);
+	return (FAIL);
+}
+
+void showArray(int *value, int size)
+{
+	for (int i = 0;i < size; i++)
+		printf("%d ", value[i]);
+	printf("\n");
+}
